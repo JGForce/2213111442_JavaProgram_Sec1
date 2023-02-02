3a1c1668d7e92637d21ce18317b69a7bb3eacc44
JGForce <forcegaysorn@gmail.com>
2023-02-02 11:59:45 +0700

    Practice 2/2/2023

--- OOP_Concept/src/MovieDemo.java
+++ OOP_Concept/src/MovieDemo.java
@@ -1,9 +1,41 @@
-
+import java.util.*;
 public class MovieDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		Theater tt = new Theater();
+		
+		System.out.print("Input movie id   : ");
+		String id = scan.nextLine();
+		System.out.print("Input movie name : ");
+	
+		System.out.println();
+		
+		System.out.print("Input director name   : ");
+		String directorName = scan.nextLine();
+		System.out.print("Input director e-mail : ");
+		String directorMail = scan.nextLine();
+		System.out.print("Input director gender : ");
+		String directorGen = scan.nextLine();
+		
+		while(directorGen.equalsIgnoreCase("m") || directorGen.equalsIgnoreCase("f")) {
+			System.out.print("Input director gender, again : ");
+			directorGen = scan.nextLine();
+		}
+		
+		System.out.println();
+		
+		System.out.print("Input movie theater no. : ");
+		int movieNumber = scan.nextInt();
+		
+		while(movieNumber < 1 || movieNumber > 16) {
+			System.out.print("Please input 1 - 15 only : ");
+			movieNumber = scan.nextInt();
+		}
+		
+		Theater tt = new Theater(directorName,directorMail,directorGen.charAt(0))
+		
 	}
 
 }
--- OOP_Concept/src/Theater.java
+++ OOP_Concept/src/Theater.java
@@ -1,4 +1,34 @@
 
-public class Theater {
+public class Theater extends Movie{
+	
+	private int theaterNo ;
+	public Theater() {
+		super();
+		this.theaterNo=0;
+	}
+	public Theater(String id, String name, Director director, int theaterNo) {
+		super(id,name,director);
+		this.theaterNo = theaterNo;
+	}
+	
+	public String getTheaterName() {
+		if (theaterNo >= 1 && theaterNo <= 11) {
+			return "Basic Theater";
+		}
+		else if (theaterNo >= 12 && theaterNo <= 14) {
+			return "Sweet Theater";
+		}
+		else if (theaterNo == 15) {
+			return "Premium Theater";
+		}
+		else {
+			return "";
+		}
+	}
+	
+	public String toString() {
+		return  getTheaterName() + ":" 	+ super.toString(); 
+	}
+
 
 }
