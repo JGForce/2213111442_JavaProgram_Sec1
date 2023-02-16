1183a7f11ca7b78d0bd29e7975f2a5585d3b4fda
JGForce <forcegaysorn@gmail.com>
2023-02-16 11:54:04 +0700

    BABFEUKHUD 16/02/23

---
+++ File/src/ATMBanking.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class ATMBanking {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter account number : ");
+		String accountNumber = scan.next();
+		while() {
+			System.out.print("Input wrong type, Enter account number : ");
+			accountNumber = scan.next();
+		}
+		System.out.print("Enter password : ");
+		String password = scan.next();
+		while() {
+			System.out.print("Enter password : ");
+			password = scan.next();
+		}
+		System.out.print("Enter money : ");
+		int money = scan.nextInt();
+		while() {
+			System.out.print("Enter money : ");
+			money = scan.nextInt();
+		}
+		
+	
+	}
+	
+}
---
+++ File/src/ATMChecking.java
@@ -0,0 +1,19 @@
+
+public class ATMChecking {
+	
+	private int money;
+	
+	public ATMChecking() {
+		
+	}
+	
+	public boolean checkBookBank() {
+		return false;
+		
+	}
+	
+	public void show() {
+		
+	}
+
+}
